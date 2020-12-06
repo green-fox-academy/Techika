b755346566926c784417ced17880e574fa0f57f8
Techika <adibukovi@gmail.com>
2020-12-06 22:45:46 +0100

    change-elements

---
+++ Week-03/HomeWorks/change-element.ts
@@ -0,0 +1,7 @@
+// -  Create an array named `numbers` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the value of the fourth element (8) to 4 with the `.map` method 
+// -  Print the fourth element as a test
+
+let numbers:number[]=[1, 2, 3, 8, 5, 6];
+
+console.log(numbers.map(element => element/2)[3]);
