d2947543af9d49abaf953f1705c73c8f99aeb64c
Techika <adibukovi@gmail.com>
2020-12-06 23:06:02 +0100

    append-a

---
+++ Week-03/HomeWorks/append-a.ts
@@ -0,0 +1,10 @@
+// -  Create an array variable named `animals`
+//    with the following content:
+//    `["koal", "pand", "zebr", "anacond", "bo", "chinchill", "cobr", "gorill",
+//      "hyen", "hydr", "iguan", "impal", "pum", "tarantul", "pirahn"]`
+//
+// -  Add all elements an `"a"` at the end
+
+let animals:string[]=["koal", "pand", "zebr", "anacond", "bo", "chinchill", "cobr", "gorill","hyen", "hydr", "iguan", "impal", "pum", "tarantul", "pirahn"];
+animals=animals.map(a => a+="a");
+console.log(animals);
