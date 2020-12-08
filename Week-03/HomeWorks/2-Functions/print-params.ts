a6096bacc68a4f302920dac2387ff5882207e253
Techika <adibukovi@gmail.com>
2020-12-08 21:32:01 +0100

    print params

---
+++ Week-03/HomeWorks/2-Functions/print-params.ts
@@ -0,0 +1,10 @@
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams(...stuff):void{
+  console.log(...stuff);
+  
+}
+
+printParams(2,'tibi','+',3);
