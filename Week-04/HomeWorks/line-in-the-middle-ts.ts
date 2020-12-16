226faf1e94dd208f0038d6b653d87edefe6c8117
Techika <adibukovi@gmail.com>
2020-12-16 22:57:25 +0100

    line in the middle

---
+++ Week-04/HomeWorks/line-in-the-middle-ts.ts
@@ -0,0 +1,19 @@
+  
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// draw a red horizontal line to the canvas' middle.
+ctx.moveTo(0,canvas.height/2);
+ctx.beginPath;
+ctx.lineTo(canvas.width,canvas.height/2);
+ctx.stroke();
+// draw a green vertical line to the canvas' middle.
+ctx.moveTo(canvas.width/2,0);
+ctx.beginPath;
+ctx.lineTo(canvas.width/2,canvas.height);
+ctx.stroke();
