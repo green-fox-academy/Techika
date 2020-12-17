eaa0151fff78029a227138823c1b435ad96a8b4a
Techika <adibukovi@gmail.com>
2020-12-17 16:39:12 +0100

    Triangles

---
+++ Week-04/HomeWorks/Triangles.ts
@@ -0,0 +1,50 @@
+'use strict';
+
+import { createTypeAliasDeclaration } from "typescript";
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Fill the canvas with a checkerboard pattern.
+
+/* Preparation: 
+I need 3 coordinates for the 3 corners and one line ratio
+drawing lines between them.. will be an interesting solution...
+
+*/
+function Piramid(
+  canvas:HTMLCanvasElement,
+  ctx:CanvasRenderingContext2D,
+  pA:[number,number]=[canvas.width/2,0],
+  pB:[number,number]=[canvas.width,canvas.height],
+  pC:[number,number]=[0,canvas.height],
+  lineRatio:number=21,
+  ){
+    ctx.moveTo(...pA);
+    ctx.beginPath;
+    ctx.lineTo(...pB);
+    ctx.lineTo(...pC);
+    ctx.closePath();
+    ctx.stroke();
+    for (let lines=1; lines<=lineRatio ; lines++){
+      let odaX0:number=pA[0] - ((pA[0]-pC[0]) / lineRatio)*lines;
+      let odaY0:number=pA[1] + ((pC[1]-pA[1]) / lineRatio)*lines;
+      let odaX1:number=pA[0] + ((pB[0]-pA[0]) / lineRatio)*lines;
+      let odaY1:number=pA[1] + ((pB[1]-pA[1]) / lineRatio)*lines;
+      let odaX2:number=pC[0] + ((pB[0]-pC[0]) / lineRatio)*(lineRatio-lines);
+      let odaY2:number=pC[1] + ((pB[1]-pC[1]) / lineRatio)*(lineRatio-lines);
+      let odaX3:number=pB[0] - ((pB[0]-pA[0]) / lineRatio)*lines;//(lineRatio-lines);
+      let odaY3:number=pB[1] - ((pB[1]-pA[1]) / lineRatio)*lines;//(lineRatio-lines);
+      //console.log(odaX0,odaY0,odaX1,odaY1);
+      ctx.moveTo(odaX1,odaY1);
+      ctx.lineTo(odaX0,odaY0);
+      ctx.lineTo(odaX2,odaY2);
+      ctx.lineTo(odaX3,odaY3);
+      ctx.stroke();
+    } 
+}
+
+Piramid(canvas,ctx);
