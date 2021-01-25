d859b6857bd378be5cf184ee2fa98bacc0880645
Techika <adibukovi@gmail.com>
2021-01-25 23:19:50 +0100

    sumdigits

---
+++ Week-06/Recursion/sumdigits.ts
@@ -0,0 +1,5 @@
+//Given a non-negative integer n, return the sum of its digits recursively (without loops).
+// There is no integer type in JavaScript. To remove the rightmost digit you must divide (/)
+// the number by 10 and find a way to get the the whole number portion of that number.
+
+function sumDigits(num: number) {}
