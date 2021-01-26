b5eb287722fa148b9f27e72d529776135edd4e7a
Techika <adibukovi@gmail.com>
2021-01-27 00:19:50 +0100

    not started

---
+++ Week-06/Recursion/maxSum_toDo.ts
@@ -0,0 +1,4 @@
+//Create a function called maxSum which expects an array of five integers as an input parameter
+//and returns the maximum values that can be calculated by summing exactly four of the five integers.
+
+function maxSum(arr5: [number, number, number, number, number]) {}
