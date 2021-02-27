dc75d10956fbac851662aacfd45fd633c24e4ee4
Techika <adibukovi@gmail.com>
2021-02-27 11:24:37 +0100

    env updates

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.ts'] }],
+    '@typescript-eslint/indent': [2, 2],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'linebreak-style': 0,
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-inferrable-types': {
+      ignoreProperties: false,
+      ignoreParameters: false,
+    },
+  },
+};
