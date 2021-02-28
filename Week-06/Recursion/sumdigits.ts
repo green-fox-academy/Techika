2c695c642cf5af83c2aa4007426253fab9c47789
Techika <adibukovi@gmail.com>
2021-02-28 18:34:58 +0100

    started node onload generation

--- Week-06/Recursion/sumdigits.ts
+++ Week-06/Recursion/sumdigits.ts
@@ -2,4 +2,14 @@
 // There is no integer type in JavaScript. To remove the rightmost digit you must divide (/)
 // the number by 10 and find a way to get the the whole number portion of that number.
 
-function sumDigits(num: number) {}
+function sumDigits(num: number): number {
+  if (num % 10 === 0) return num / 10;
+  else if (Math.floor(num / 10) === 0) return num % 10;
+  return (num % 10) + (sumDigits(Math.floor(num / 10)) % 10);
+}
+
+console.log(sumDigits(15));
+console.log(sumDigits(10));
+console.log(sumDigits(5));
+console.log(sumDigits(20));
+console.log(sumDigits(3678));
--- Week-11-ImageSlider/index.js
+++ Week-11-ImageSlider/index.js
@@ -2,8 +2,31 @@
 //   'use strict';
 
 // });
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const fs = require('fs');
-const pictureNames = fs.readdirSync('./gallery');
-console.log(pictureNames);
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+// const fs = require('fs');
+// const pictureNames = fs.readdirSync('./gallery');
+// console.log(pictureNames);
+
+const imageNames = ['oaktree.jpg', 'bunny.jpg', 'fox.jpg', 'gekko.jpg', 'hotdog.jpg'];
+window.onload = function () {
+  const thumbnailTemplate = document.getElementById('thumbnail_0');
+  imageNames.forEach((picture, index) => {
+    const tempNode = thumbnailTemplate.cloneNode(true);
+    tempNode.getElementsByClassName('arrow-up').id = 'upArrow_' + 1;
+  });
+  document.getElementById('thumbnailContainer').appendChild(thumbnailTemplate.cloneNode(true));
+  document.getElementById('thumbnailContainer').appendChild(thumbnailTemplate.cloneNode(true));
+  document.getElementById('thumbnailContainer').appendChild(thumbnailTemplate.cloneNode(true));
+  document.getElementById('thumbPic_0').src = './gallery/oaktree.jpg';
+};
+
+// thumbnailTemplate.getElementById('thumbPic_0').src = 'gallery/oaktree.jpg';
+// document.getElementById("imageid").src="../template/save.png";
+function pageLeft() {
+  alert('scroll left');
+}
+
+function pageRight() {
+  alert('scroll right');
+}
