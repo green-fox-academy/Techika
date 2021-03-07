94baf85077dfece91c43a9a632ed2f5f691cea3b
Techika <adibukovi@gmail.com>
2021-03-07 21:45:53 +0100

    Moonstar add randoms10 | add onlclick

---
+++ Week-11-DOM/moonStar/script.js
@@ -0,0 +1,24 @@
+const starTemplate = document.querySelector('.star').cloneNode();
+
+for (let index = 0; index < 10; index++) {
+  const randW = randBetween();
+  const randH = randBetween();
+  const newClone = starTemplate.cloneNode();
+  newClone.style.left = `${randW}%`;
+  newClone.style.top = `${randH}%`;
+  document.querySelector('.world').appendChild(newClone);
+}
+
+function randBetween(min = 1, max = 99) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+document.querySelector('.world').addEventListener('click', worldClicked, true);
+
+function worldClicked(e) {
+  const newClone = starTemplate.cloneNode();
+  newClone.style.left = `${e.clientX}px`;
+  newClone.style.top = `${e.clientY}px`;
+  document.querySelector('.world').appendChild(newClone);
+}
