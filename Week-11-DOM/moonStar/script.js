024ba02a907fc750c0200d34c285aea675b17ab8
Techika <adibukovi@gmail.com>
2021-03-07 23:11:41 +0100

    moonStar fixes + treeGrow

--- Week-11-DOM/moonStar/script.js
+++ Week-11-DOM/moonStar/script.js
@@ -15,12 +15,13 @@ function randBetween(min = 1, max = 99) {
 }
 
 document.querySelector('.world').addEventListener('click', worldClicked, true);
-
 function worldClicked(e) {
-  const newClone = starTemplate.cloneNode();
-  newClone.style.left = `${e.clientX}px`;
-  newClone.style.top = `${e.clientY}px`;
-  document.querySelector('.world').appendChild(newClone);
+  if (e.clientY < 800) {
+    const newClone = starTemplate.cloneNode();
+    newClone.style.left = `${e.clientX}px`;
+    newClone.style.top = `${e.clientY}px`;
+    document.querySelector('.world').appendChild(newClone);
+  }
 }
 
 (function randomFall() {
@@ -28,7 +29,16 @@ function worldClicked(e) {
   var rand = randBetween(500, 3000);
   setTimeout(function () {
     const stars = document.querySelectorAll('.star:not(.fallen)');
-    stars[randBetween(0, stars.length - 1)].classList.add('fallen');
-    randomFall();
+    if (stars.length !== 0) {
+      stars[randBetween(0, stars.length - 1)].classList.add('fallen');
+      randomFall(); // if you need the process looping, move this line out of the if
+    }
   }, rand);
 })();
+
+document.querySelectorAll('.tree').forEach((tree) => {
+  tree.addEventListener('click', growNode, true);
+});
+function growNode(e) {
+  e.currentTarget.appendChild(document.createElement('span'));
+}
