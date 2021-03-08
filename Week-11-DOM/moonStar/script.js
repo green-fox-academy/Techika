2e9c4acd249fe758f7d8267337837b8c888b9e3e
Techika <adibukovi@gmail.com>
2021-03-08 16:10:07 +0100

    moonStar final: - proximity sensor + CSS tweeks

--- Week-11-DOM/moonStar/script.js
+++ Week-11-DOM/moonStar/script.js
@@ -1,48 +1,81 @@
 const starTemplate = document.querySelector('.star').cloneNode();
 
+//--- Adding 10 extra stars at random locations (above ground)
 for (let index = 0; index < 10; index++) {
-  const randW = randBetween();
-  const randH = randBetween();
-  const newClone = starTemplate.cloneNode();
-  newClone.style.left = `${randW}%`;
-  newClone.style.top = `${randH}%`;
-  document.querySelector('.world').appendChild(newClone);
+  const randX = randBetween();
+  const randY = randBetween();
+  if (noStarsIn50px(randX, randY)) {
+    const newClone = starTemplate.cloneNode();
+    newClone.style.left = `${randX}%`;
+    newClone.style.top = `${randY}%`;
+    document.querySelector('.world').appendChild(newClone);
+  } else {
+    index -= 1;
+  }
 }
 
 function randBetween(min = 1, max = 99) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-document.querySelector('.world').addEventListener('click', worldClicked, true);
-function worldClicked(e) {
+//--- Adding star on click location ----
+document.querySelector('.world').addEventListener('click', addStarOnClick, true);
+function addStarOnClick(e) {
   if (e.clientY < 800 && !e.target.classList.contains('moon')) {
     const newClone = starTemplate.cloneNode();
-    newClone.style.left = `${e.clientX}px`;
-    newClone.style.top = `${e.clientY}px`;
-    document.querySelector('.world').appendChild(newClone);
+    if (noStarsIn50px(e.clientX, e.clientY)) {
+      newClone.style.left = `${e.clientX}px`;
+      newClone.style.top = `${e.clientY}px`;
+      document.querySelector('.world').appendChild(newClone);
+    }
   }
 }
 
+//---  proximity prevention
+function noStarsIn50px(newX, newY) {
+  const stars = document.querySelectorAll('.star:not(.fallen)');
+  let output = true;
+  if (stars.length !== 0) {
+    return Array.from(stars).every((star) => {
+      const oldX = star.getBoundingClientRect().x;
+      const oldY = star.getBoundingClientRect().y;
+      const distance = Math.sqrt(Math.pow(newY - oldY, 2) + Math.pow(newX - oldX, 2));
+      console.log(distance);
+      if (distance <= 50) {
+        return false;
+      }
+      return true;
+    });
+  } else {
+    return true;
+  }
+}
+
+//--- Falling stars randomly 1by1 and removing them after fall animation
 (function randomFall() {
-  console.log('started');
   var rand = randBetween(500, 3000);
   setTimeout(function () {
+    const fallen = document.querySelector('.star:is(.fallen)');
+    if (fallen) {
+      fallen.remove();
+    }
     const stars = document.querySelectorAll('.star:not(.fallen)');
     if (stars.length !== 0) {
-      stars[randBetween(0, stars.length - 1)].classList.add('fallen');
+      const randomStar = stars[randBetween(0, stars.length - 1)];
+      randomStar.classList.add('fallen');
       randomFall(); // if you need the process looping, move this line out of the if
     }
   }, rand);
 })();
 
+//--- Makingtrees grow on click
 document.querySelectorAll('.tree').forEach((tree) => {
   tree.addEventListener('click', growNode, true);
 });
 function growNode(e) {
   e.currentTarget.appendChild(document.createElement('span'));
 }
-//--- stuff for dragging
+//--- Making Moon draggable
 const moon = document.querySelector('.moon');
 moon.onmousedown = function (event) {
   let shiftX = event.clientX - moon.getBoundingClientRect().left;
@@ -71,4 +104,4 @@ moon.onmousedown = function (event) {
 moon.ondragstart = function () {
   return false;
 };
-// end of dragging stuff
+//----------------------------
