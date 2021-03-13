80c8235ae808cabc828714010715967e444f8a15
Techika <adibukovi@gmail.com>
2021-03-13 23:49:20 +0100

    giphy init

---
+++ Week-13-XHR/giphy-api.js
@@ -0,0 +1,7 @@
+// Display gifs of a cute/funny topic using: https://developers.giphy.com/docs/
+// Obtain an API key
+// Search/Find the images in the API
+// Display the list of the first 16 results' static thumbnail
+// If the user clicks on the thumbnail, display the animated GIF
+
+// Naah I don't want to create a giphy account to solve this excercise, skipping for now.
