6fe8402e4722ff3fdd3962720eab33a6465739a8
Techika <adibukovi@gmail.com>
2021-03-20 23:50:49 +0100

    bookServer | Complete with extras

--- Week-14-NodeQL/bookstore/bookServer.js
+++ Week-14-NodeQL/bookstore/bookServer.js
@@ -1,14 +1,14 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const mysql = require('mysql');
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '2EFWg#z!jgu329',
-  database: 'bookstore',
-});
+import * as mysql from 'mysql';
+// const db = mysql.createConnection({
+//   // host: 'localhost',
+//   // user: 'root',
+//   // password: '2EFWg#z!jgu329',
+//   // database: 'bookstore',
+// });
 
-let htmlCore = (body) => {
+const htmlCore = (results) => {
   return `
 <!DOCTYPE html>
 <html lang="en">
@@ -16,45 +16,109 @@ let htmlCore = (body) => {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Book Titles</title>
+    <title>Book Store</title>
+    <style>
+      th {
+        background-color: grey;
+      }
+      td {
+        padding:5px;
+      }
+      tr:nth-child(odd) {
+        background-color: lightgrey;
+      }
+    </style>
   </head>
   <body>
     <div class="notecontainer">
-      ${body}
+      <p><u>BUK's BOOK STORE:</u></p><br>
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Category</th>
+          <th>Publisher</th>
+          <th>Price</th>
+        </tr>
+        ${results}
+      </table>
     </div>
   </body>
 </html>
 `;
 };
+const htmlResultRow = (data) => {
+  return `
+  <tr>
+    <td>${data.title}</td>
+    <td>${data.author}</td>
+    <td>${data.category}</td>
+    <td>${data.publisher}</td>
+    <td>${data.price}</td>
+  </tr>
+`;
+};
 
-// app.use('/', express.static('assets')); // serving static files // middleware
 app.get('/', (req, res) => {
   res.send("I'm alive");
 });
+
 app.get('/bookstore', (req, res) => {
-  res.send('This is the homepage');
-});
-app.get('/bookstore/titles', (req, res) => {
-  db.query('SELECT book_name FROM bookstore.book_mast;', (err, rows) => {
-    if (err) {
-      console.error(`Cannot retrieve data: ${err.toString()}`);
-      res.sendStatus(500);
-      return null;
-    }
-    db.end();
-    let htmlBody = '';
-    //let booklist = rows.map((row) => row.book_name).foreach((book) => {
-    let booklist = rows.forEach((row) => {
-      htmlBody += `<p>${row.book_name}</p>`;
-    });
-    // console.log(booklist);
-    return res.send(htmlCore(htmlBody)); //res.send(rows);
+  const db = mysql.createConnection({
+    host: 'buk.b2center.de',
+    user: 'd035d455',
+    password: '5mmuYV8hfG8Rop9P',
+    database: 'd035d455',
   });
+  db.query(
+    `
+    SELECT 
+      master.*, 
+      author.aut_name author,
+      category.cate_descrip category,
+      publisher.pub_name publisher
+    FROM book_mast AS master
+      INNER JOIN author ON master.aut_id = author.aut_id
+      INNER JOIN category ON master.cate_id = category.cate_id
+      INNER JOIN publisher ON master.pub_id = publisher.pub_id
+    HAVING
+      category like "%${req.query.category || ''}%" AND
+      publisher like "%${req.query.publisher || ''}%" AND
+      book_name like "%${req.query.title || ''}%" AND
+      author like "%${req.query.author || ''}%" AND
+      book_price BETWEEN ${req.query.pgt || 0} AND ${req.query.plt || 10000};
+  ;`,
+    (err, rows) => {
+      if (err) {
+        console.error(`Cannot retrieve data: ${err.toString()}`);
+        res.sendStatus(500);
+        return null;
+      }
+      db.end();
+      let results = '';
+      rows.forEach((row) => {
+        results += htmlResultRow({
+          title: row.book_name,
+          category: row.category,
+          author: row.author,
+          publisher: row.publisher,
+          price: row.book_price,
+        });
+      });
+      if (req.query.category || req.query.publisher || req.query.pgt || req.query.plt) {
+        results += htmlResultRow({
+          title: `<b>Filters:${req.query.title || '__'}</b>`,
+          author: `<b>${req.query.author || '__'}</b>`,
+          category: `<b>${req.query.category || '__'}</b>`,
+          publisher: `<b>${req.query.publisher || '__'}</b>`,
+          price: `<b>${req.query.pgt || '0'} < ${req.query.plt || 'oo'}</b>`,
+        });
+      }
+      return res.send(htmlCore(results));
+    }
+  );
 });
-app.get('/bookstore/test', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end(htmlCore);
-});
+
 //------------------------------------------------------------
 app.listen(8080);
 // ------------------------------------------------------------
