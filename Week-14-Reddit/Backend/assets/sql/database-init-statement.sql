99a444ff9cfcc94c2b37f7f8d7f76c07d34d6b29
Techika <adibukovi@gmail.com>
2021-04-03 01:06:45 +0200

    REddit-backed dbInit backup

---
+++ Week-14-Reddit/Backend/assets/sql/database-init-statement.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE `reddit` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+CREATE TABLE `posts` (
+  `post_id` int NOT NULL AUTO_INCREMENT COMMENT '\n',
+  `title` varchar(45) NOT NULL,
+  `url` varchar(250) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `owner_id` int DEFAULT NULL,
+  `score` int DEFAULT '0',
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`post_id`),
+  UNIQUE KEY `title_UNIQUE` (`title`),
+  KEY `user_id_idx` (`owner_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`owner_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `login_email` varchar(45) NOT NULL,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `registration_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `password` varchar(45) DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `login_email_UNIQUE` (`login_email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `votes` (
+  `user_id` int NOT NULL,
+  `post_id` int NOT NULL,
+  `vote` decimal(1,0) DEFAULT '0',
+  PRIMARY KEY (`user_id`,`post_id`),
+  KEY `post_id_idx` (`post_id`),
+  CONSTRAINT `post_id` FOREIGN KEY (`user_id`) REFERENCES `posts` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
